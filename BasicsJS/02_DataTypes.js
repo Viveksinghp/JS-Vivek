cfef955c4ee6041afc2b457ca00374fadf096047
Vivek Singh <viveksingh2k04@gmail.com>
2026-07-18 06:10:32 +0000

    datatypes

---
+++ BasicsJS/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as never verion
+
+//alert (3+3)// we are using nodejs not browser
+
+
+let name="Vivek"
+let age=22
+let isLoggedIn=false
+let state;
+
+//number => 2 to power 53
+//bigint
+//String => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique    
+
+//object
+
+
+console.log(typeof age) // numvere
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
