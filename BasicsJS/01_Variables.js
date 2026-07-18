9b0a373c5ad362f93062d0d7b0ac16b2eedd1641
Vivek Singh <viveksingh2k04@gmail.com>
2026-07-18 06:10:00 +0000

    variables

---
+++ BasicsJS/01_Variables.js
@@ -0,0 +1,17 @@
+const accountID =154555
+let accountEmail="viveksinghglv@gmail.com";
+var accountPAssword="14465374"
+
+accountCity = "Jaipur"
+// accounyID=4 // not allowed to modify const keyword
+
+console.log(accountID);
+accountPAssword='56556556'
+accountCity='Varanasi'
+
+console.table([accountID,accountEmail,accountPAssword,accountCity])
+
+/*
+Dot Not Use Var keyword its not good at all.
+
+*/
