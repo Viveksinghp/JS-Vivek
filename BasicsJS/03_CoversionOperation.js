243bb74c8353072eba1a2759100f0186b81137f5
Vivek Singh <viveksingh2k04@gmail.com>
2026-07-18 06:38:37 +0000

    CoversionDatatypes

---
+++ BasicsJS/03_CoversionOperation.js
@@ -0,0 +1,20 @@
+// let score =33
+// console.log(typeof score);
+// console.log(typeof (score) );
+
+// let valueInNumber= Number (score)
+// console.log(typeof valueInNumber); 
+
+// "33" =>33 
+// "33abbs" => NaN
+// true=> 1, false=>0
+
+
+let isLoggendIn=1
+let booleanIsLoggedIn = Boolean(isLoggendIn)
+console.log(booleanIsLoggedIn);
+
+let sumnumber=13
+let stringNumber= String(sumnumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
