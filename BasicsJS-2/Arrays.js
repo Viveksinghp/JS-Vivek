f7008b00827eb59717e03f0fc319ce9d4e8db385
Vivek Singh <viveksingh2k04@gmail.com>
2026-07-19 08:41:56 +0000

    Arrays

---
+++ BasicsJS-2/Arrays.js
@@ -0,0 +1,34 @@
+// Arrays
+
+const  Myarr=[0,1,2,3,4,5,6]
+const myHeros = ["shaktiman","naagraj"]
+
+const myArr2 = new Array (1,2,3,4)
+console.log(myArr2[1]);
+
+//Arrays method
+// myArr2.push(6)
+// myArr2.push(7)
+// myArr2.pop()
+// console.log(myArr2)
+
+// myArr2.unshift(9)
+// myArr2.shift()
+// console.log(myArr2)
+
+
+// console.log(myArr2.includes(9))
+// console.log(myArr2.indexOf(9))
+
+const newArr = myArr2.join()
+
+console.log(myArr2);
+console.log(newArr);
+
+//splice plice
+
+console.log("A",myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr2)
