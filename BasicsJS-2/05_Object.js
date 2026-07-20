016b126514d2e575fdddc9c8797f09b0f3406498
Vivek Singh <viveksingh2k04@gmail.com>
2026-07-20 06:21:35 +0000

    Object & json

---
+++ BasicsJS-2/05_Object.js
@@ -0,0 +1,24 @@
+// Destrcuturing
+const course = {
+    coursename:"jS in hindi",
+    price: "999",
+    courseIntruct: "Hitesh"
+}
+course.courseIntruct
+
+const {courseIntruct:inst} = course
+
+//console.log(courseIntruct)
+console.log(inst) // di structure kar diya gya name coursename ka
+
+
+// {
+//     name: "vivek",
+//     "coursename":"js in hindi",
+//     price:"free"
+// }
+[
+    {},
+    {},
+    {}
+]
