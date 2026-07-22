28f08bb6872509e4d69bdc36251e16dc5e4f8a4b
Vivek Singh <viveksingh2k04@gmail.com>
2026-07-22 07:34:34 +0000

    truthy

---
+++ Conditional/truthy.js
@@ -0,0 +1,45 @@
+// const userEmail ="vivek@gmail.com"
+
+// if(userEmail){
+//     console.log("Got user email");
+// }
+// else {
+//     console.log("Don't have user email");
+// }
+
+// falsy values
+
+//false , 0 ,- 0 , BigInt 0n ,"" ,null , undefined ,NaN
+
+//true values
+
+
+// "0" , 'false' , " " ,[] ,{}, function(){}
+
+// const emailID=[]
+// if(emailID.length === 0){
+//     console.log("Empty Array")
+// }
+// const emptObj ={}
+// if(Object.keys(emptObj).length === 0){
+//     console.log("Empty Object")
+// }
+
+//Nullish Coalescing operator (??): null undefined
+
+let val1;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+
+
+
+
+
+console.log(val1)
+
+// Terniary Operator
+
+// Condition ? true : false
+
+// const iceTeaPrice = 100
+// iceTeaPrice >= 80? console.log
